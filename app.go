75b0e28a18e754b10f40e042b662971ac50d720e
Wieland Hoffmann <themineo@gmail.com>
2017-07-01 12:43:14 +0200

    Add a basic json interface

--- app.go
+++ app.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 	"os"
-	"html/template"
 	//for extracting service credentials from VCAP_SERVICES
 	//"github.com/cloudfoundry-community/go-cfenv"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"strconv"
 )
 
 const (
@@ -14,12 +17,71 @@ const (
 )
 
 var index = template.Must(template.ParseFiles(
-  "templates/_base.html",
-  "templates/index.html",
+	"templates/_base.html",
+	"templates/index.html",
 ))
 
+type StatusCodeDescription struct {
+	Code        int
+	Description string
+}
+
+type Response struct {
+	Error    string
+	Response StatusCodeDescription
+}
+
 func helloworld(w http.ResponseWriter, req *http.Request) {
-  index.Execute(w, nil)
+	index.Execute(w, nil)
+}
+
+func writeJSON(w http.ResponseWriter, code int, obj interface{}) (err error) {
+	var bytes []byte
+
+	if bytes, err = json.Marshal(obj); err != nil {
+		log.Println(err.Error())
+	}
+
+	w.WriteHeader(code)
+	_, _ = w.Write(bytes)
+	return
+}
+
+func describeHTTPStatusCode(w http.ResponseWriter, req *http.Request) {
+	var response Response
+
+	vars := mux.Vars(req)
+	statuscode, err := strconv.Atoi(vars["statuscode"])
+
+	if err != nil {
+		response.Error = err.Error()
+		log.Println(err.Error())
+		_ = writeJSON(w, http.StatusBadRequest, response)
+		return
+	}
+
+	description := http.StatusText(statuscode)
+
+	scd := StatusCodeDescription{Code: statuscode, Description: description}
+	response.Response = scd
+
+	var bytes []byte
+
+	if bytes, err = json.Marshal(response); err != nil {
+		response.Error = err.Error()
+		log.Println(err.Error())
+		_ = writeJSON(w, http.StatusInternalServerError, response)
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	if _, err := w.Write(bytes); err != nil {
+		response.Error = err.Error()
+		log.Println(err.Error())
+		_ = writeJSON(w, http.StatusInternalServerError, response)
+		return
+	}
+
 }
 
 func main() {
@@ -28,9 +90,10 @@ func main() {
 		port = DEFAULT_PORT
 	}
 
-	http.HandleFunc("/", helloworld)
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	
+	r := mux.NewRouter()
+	r.HandleFunc("/status/{statuscode}", describeHTTPStatusCode)
+	http.Handle("/", r)
+
 	log.Printf("Starting app on port %+v\n", port)
 	http.ListenAndServe(":"+port, nil)
 }
